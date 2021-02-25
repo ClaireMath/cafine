56ccf370c4eeb63a297b9214334666b78e71b659
alexismajchrzak <alexis.majchrzak77@gmail.com>
2021-02-25 18:47:29 +0100

    header + header css

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="header.css">
+
+<!--  <link href="../footer/footer.css" rel="stylesheet" />  -->
+    <title>Cafiné</title>
+</head>
+<body>
+
+   
+
+
+
+
+
+    <h1>Bienvenue!</h1>
+    
+    <!--- javaSciprt -->
+<script type="text/javascript"  src="header.js"  ></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+</body>
+</html>
