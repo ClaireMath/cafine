c8a790bc43ea82d5149c8882bd8d1f26e5acc2a1
alexismajchrzak <alexis.majchrzak77@gmail.com>
2021-02-26 18:22:32 +0100

    ajout gif + ajout header page index

--- index.php
+++ index.php
@@ -14,13 +14,89 @@
 </head>
 <body>
 
-   
+    <!-- navbar -->
+
+            <!-- LOGO -->
+
+            <div class="lanavbar">
+            
+            <nav class="navbar sticky-top navbar-light navbar-expand-xl bg-transparent fz-text shadow-sm d-flex align-items-center  ">
+               <div class="container-fluid">
+                   <a class="navbar-brand d-flex align-items-center" href="/cafine/index.php">
+                       <img class="ms-5" src="/cafine/header/logoCafine.png" width="130" alt="" loading="lazy">
+                   </a>
+                   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                       <span class="navbar-toggler-icon"></span>
+                   </button> 
+           
+                   <!--taille police ... -->
+           
+                   <div class="collapse navbar-collapse d-flex justify-content-end " style="font-size: 16px" id="navbarSupportedContent">
+                       <ul class="navbar-nav mb-2 mb-lg-0 me-3 d-flex flex-wrap" style="font-weight: 400 "  >    <!-- taille de la grosseur  -->
+           
+                           <form class="d-flex">
+                               <input class="form-control me-2 " type="search" placeholder="Rechercher" aria-label="Search">
+                               <button class="btn btn-outline-success  " type="submit">Valider</button>
+                             </form>
+           
+           
+           
+                           <li class="nav-item d-flex flex-column px-lg-4 ">
+                               <a class="nav-link text-dark" aria-current="page" href="/cafine/index.php">Accueil</a>
+                           </li>
+           
+                           <li class="nav-item d-flex flex-column px-lg-3">
+                           <li class="nav-item dropdown">
+                               <a class="nav-link dropdown-toggle nav-link text-dark " data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Nos produits</a>
+                               <ul class="dropdown-menu">
+                               <li><a class="dropdown-item" href="#">Thé</a></li>
+                               <li><a class="dropdown-item" href="#">Café</a></li>
+                               <li><a class="dropdown-item" href="#">alcool</a></li>
+                               <li><hr class="dropdown-divider"></li>             <!-- ligne séparatrice -->
+                               <li><a class="dropdown-item  " href="#">achat en ligne</a></li>
+                           </ul>
+                           </li>
+                           </li>
+           
+           
+                           <li class="nav-item d-flex flex-column px-lg-5">
+                               <a class="nav-link text-dark" href="">Nous </a>
+                           </li>
+                           </li>
+           
+                           <li class="nav-item d-flex flex-column px-lg-4">
+                               <a class="nav-link text-dark" href="">Acheter en ligne</a>
+                           </li>
+                           </li>
+                   </div>
+               </div>
+              </nav>
+           
+           </div>
+           
+           
+                   <!-- fin Navbar  -->
 
 
 
 
 
-    <h1>Bienvenue!</h1>
+                   beinvenue c'est l'index
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
     <!--- javaSciprt -->
 <script type="text/javascript"  src="header.js"  ></script>
