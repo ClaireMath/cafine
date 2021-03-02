14b55b081b423bbc667d0bb05d07e8355324fdee
alexismajchrzak <alexis.majchrzak77@gmail.com>
2021-03-02 16:06:14 +0100

    ajout logo e-commerce

--- achat/achat.php
+++ achat/achat.php
@@ -5,8 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="../header/header.css">
+    <link rel="stylesheet" href="styleFooter.css">
+
+    
 
     <link rel="stylesheet" href="achat.css">
 
@@ -25,6 +26,16 @@
     include "../header/headerBoutique.html"
     ?>
 
+<div class="titre " >
+    <img src="tea.png " style="width:3%;">
+    <img src="graineCafe.png"  style="width:3% ; " >
+    <img src="chocolat.png" style="width:3% ; " >
+    <img src="poivre.png" style="width:3% ; " >
+    <img src="autres.png" style="width:4% ; " >
+
+</div>
+
+
 
     <!-- ajout de card  -->
 
@@ -33,8 +44,6 @@
 
             <div class="col">
                 <div class="card h-100">
-
-
                     <div class="container">
                         <img src="/cafine/IMAGES_CL/coffeeGIF/cafe2.gif" class="card-img-top" alt="..." data-bs-toggle="modal" data-bs-target="#exampleModal  ">
                         <div class="overlay">Ajouter aux panier <img src="panier.png " style="width: 11%;" alt="panier"> </div>
@@ -80,9 +89,8 @@
 
             <div class="col">
                 <div class="card h-100">
-
                     <div class="container">
-                        <img src="/cafine/IMAGES_CL/coffeeGIF/cafe2.gif" class="card-img-top" alt="...">
+                        <img src="/cafine/IMAGES_CL/coffeeGIF/cafe2.gif" class="card-img-top" alt="..." data-bs-toggle="modal" data-bs-target="#exampleModal  ">
                         <div class="overlay">Ajouter aux panier <img src="panier.png " style="width: 11%;" alt="panier"> </div>
 
                         <!-- ajout fenetre panier  -->
@@ -98,7 +106,99 @@
                                         <img class="ajoutpanier" src="/cafine/IMAGES_CL/coffeeGIF/cafe2.gif" style="width: 30%; ">
                                     </div>
 
-                                    <h5 class="titrePanier" > Voici le premier café </h5>
+                                    <h5 class="titrePanier" > Voici le deuxième café </h5>
+
+                                    <div class="modal-footer">
+                                        <p class="prixAjout "> Prix : 10€ </p>
+                                        <div>
+                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
+                                            <button type="button" class="btn btn-primary">Ajouter</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- fin ajout fenetre panier  -->
+
+                    </div>
+                    <div class="card-body ">
+                        <h5 class="card-title">Voici le deuxième café</h5>
+                        <p class="card-text"> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt, aut error veritatis facilis voluptatem blanditiis aliquam provident harum ipsam! Unde, amet delectus? Dignissimos porro iste dolorem cupiditate vel recusandae vero. </p>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+
+            <div class="col">
+                <div class="card h-100">
+                    <div class="container">
+                        <img src="/cafine/IMAGES_CL/coffeeGIF/cafe2.gif" class="card-img-top" alt="..." data-bs-toggle="modal" data-bs-target="#exampleModal  ">
+                        <div class="overlay">Ajouter aux panier <img src="panier.png " style="width: 11%;" alt="panier"> </div>
+
+                        <!-- ajout fenetre panier  -->
+                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Êtes vous sur de rajouter cette arcticle à votre panier? </h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+
+                                        <img class="ajoutpanier" src="/cafine/IMAGES_CL/coffeeGIF/cafe2.gif" style="width: 30%; ">
+                                    </div>
+
+                                    <h5 class="titrePanier" > Voici le 3ème café </h5>
+
+                                    <div class="modal-footer">
+                                        <p class="prixAjout "> Prix : 30 € </p>
+                                        <div>
+                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
+                                            <button type="button" class="btn btn-primary">Ajouter</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- fin ajout fenetre panier  -->
+
+                    </div>
+                    <div class="card-body ">
+                        <h5 class="card-title">Voici le troisième café</h5>
+                        <p class="card-text"> t, aut error veritatis facilis voluptatem blanditiis aliquam provident harum ipsam! Unde, amet delectus? Dignissimos porro iste dolorem cupiditate vel recusandae vero. </p>
+                    </div>
+                </div>
+            </div>
+
+
+
+
+        
+
+            <div class="col">
+                <div class="card h-100">
+                    <div class="container">
+                        <img src="/cafine/IMAGES_CL/coffeeGIF/cafe2.gif" class="card-img-top" alt="..." data-bs-toggle="modal" data-bs-target="#exampleModal  ">
+                        <div class="overlay">Ajouter aux panier <img src="panier.png " style="width: 11%;" alt="panier"> </div>
+
+                        <!-- ajout fenetre panier  -->
+                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Êtes vous sur de rajouter cette arcticle à votre panier? </h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+
+                                        <img class="ajoutpanier" src="/cafine/IMAGES_CL/coffeeGIF/cafe2.gif" style="width: 30%; ">
+                                    </div>
+
+                                    <h5 class="titrePanier" > Voici le 4ème café </h5>
 
                                     <div class="modal-footer">
                                         <p class="prixAjout "> Prix : 10 € </p>
@@ -112,51 +212,13 @@
                         </div>
                         <!-- fin ajout fenetre panier  -->
 
-                </div>
-                    <div class="card-body">
-                        <h5 class="card-title">Voici 2eme café </h5>
-                        <p class="card-text"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque beatae ea exercitationem asperiores corporis veritatis voluptatibus ullam odio quia, sint recusandae vero corrupti mollitia repudiandae laboriosam minima velit, sapiente possimus? </p>
+                    </div>
+                    <div class="card-body ">
+                        <h5 class="card-title">Voici le 4ème café</h5>lectus? Dignissimos porro iste dolorem cupiditate vel recusandae vero. </p>
                     </div>
                 </div>
             </div>
 
-
-
-
-
-            <div class="col">
-                <div class="card h-100">
-
-                    <div class="container">
-                        <img src="/cafine/IMAGES_CL/coffeeGIF/cafe2.gif" class="card-img-top" alt="...">
-                        <div class="overlay">Ajouter aux panier <img src="panier.png " style="width: 11%;" alt="panier"> </div>
-                    </div>
-
-                    <div class="card-body">
-                        <h5 class="card-title">Voici café</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-                    </div>
-                </div>
-            </div>
-
-
-
-        
-
-            <div class="col">
-                <div class="card h-100">
-
-                    <div class="container">
-                        <img src="/cafine/IMAGES_CL/coffeeGIF/cafe2.gif" class="card-img-top" alt="...">
-                        <div class="overlay">Ajouter aux panier <img src="panier.png " style="width: 11%;" alt="panier"> </div>
-                    </div>
-
-                    <div class="card-body">
-                        <h5 class="card-title">Voici 4eme article</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    </div>
-                </div>
-            </div>
         </div>
     </div>
 
@@ -164,6 +226,24 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
     <!--- javaSciprt -->
     <script type="text/javascript" src="header.js"></script>
 
