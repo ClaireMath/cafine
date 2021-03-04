d7884519eee8cd4dc4f21831705ef8362e9c1a30
Claire MATHIRON <claira.m@live.fr>
2021-03-04 18:16:22 +0100

    ajout d'un onglet blog dans header + ajout de la page blog

---
+++ blog/blog.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../header/header.css">
+    <link rel="stylesheet" href="blog.css">
+    <link rel="stylesheet" href="../FOOTER/styleFooter.css"> 
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+ <link href="../footer/footer.css" rel="stylesheet" />  
+    <title>Cafiné</title>
+</head>
+
+
+<?php 
+include "../header/header.html" 
+?>  
+    <body>
+
+
+        <div class="container">
+            <div class="frame">
+                <div id="placement" class="parallax1"></div>
+                <div class="text opacity-md-25 ">
+                    <h1>Bienvenue sur notre blog !</h1>
+                    <p>Ici vous pourrez en apprendre plus sur les spiritueux en général. </p>
+                </div>
+                <div id="placement" class="parallax2"></div>
+                <div class="text">
+                    <h3>Article sur la fabrication du rhum</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur qui unde expedita corrupti! Aperiam, provident, optio cupiditate iure esse commodi eos nihil, quibusdam consectetur ullam dolorum. Sequi eveniet repellat distinctio?</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita placeat sapiente dolor corporis laboriosam, aliquid tempore nam eaque perspiciatis aperiam, nulla repellendus? Quia facilis vitae eveniet, suscipit sunt cupiditate harum!</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus dolores ipsam nam libero fuga exercitationem obcaecati, quasi atque mollitia similique rem saepe laborum perferendis possimus? Placeat voluptatum aliquam distinctio fugit!</p>
+                </div>
+                <div id="placement" class="parallax3"></div>
+                <div class="text">
+                    <h3>Autre article</h3>
+                    <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit nesciunt dolorum in fugiat illo quis laudantium, eos incidunt deleniti, autem eaque pariatur iure unde. Incidunt aut tenetur exercitationem perferendis dolorem.</p>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt aperiam, voluptates sequi, laborum earum sed culpa excepturi iure adipisci eveniet facilis consequuntur impedit amet explicabo eius ullam optio aspernatur corporis!</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, minus nihil sunt, impedit dicta, distinctio culpa quis asperiores possimus unde iure qui officia consequuntur ullam pariatur eum! Sint, explicabo necessitatibus.</p>
+                </div>
+            </div>
+        </div>
+
+        
+        <?php
+            include "../FOOTER/footerCl.html"
+        ?>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    </body>
+
